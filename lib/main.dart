1488cad1377592bc32ec96ec911cc666ace0fbc1
Andressa Silva de Souza <andressaszx@gmail.com>
2020-03-29 14:17:17 -0300

    ajustanto falha que nao permite dar push. Atualizando o repositorio remoto para depois fazer o pull na maquina local, em seguida o push

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,59 @@ void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Contador de Pessoas",
-    home: Stack(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
         Image.asset(
-          "images/fundo.jpg",
-          fit: BoxFit.cover,
+          "images/praia.jpg",
+          fit: BoxFit.fill,
           height: 1000.0,
+          width: 2000.0,
         ),
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Pessoas: 0", style: TextStyle(fontSize: 35.0, color: Colors.amber),),
+            Text("Pessoas: 0", style: TextStyle(fontSize: 35.0, color: Colors.green[900])),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                    onPressed: (){},
-                    child: Text("+ 1", style: TextStyle(fontSize: 30.0, color: Colors.amber),),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      color: Colors.white,
+                      onPressed: (){
+                        debugPrint("+ 1");
+                      },
+                      child: Text("+ 1", style: TextStyle(fontSize: 30.0, color: Colors.green[900]),),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                    onPressed: (){},
-                    child: Text("- 1", style: TextStyle(fontSize: 30.0, color: Colors.amber),),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      color: Colors.white,
+                      onPressed: (){
+                        debugPrint("- 1");
+                      },
+                      child: Text("- 1", style: TextStyle(fontSize: 30.0, color: Colors.green[900]),),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Text("Pode entrar!", style: TextStyle(fontSize: 35.0, color: Colors.amber),),
+                ],
+              ),
+            Text("Pode entrar!", style: TextStyle(fontSize: 35.0, color: Colors.green[900]),),
           ],
         ),
       ],
-    ),
-  ));
+    );
+  }
 }
